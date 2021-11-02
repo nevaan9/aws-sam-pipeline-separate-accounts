d52202bf69cf62ae04160b27d0280034d3b320f8
Nevaan Perera <nevaan9@gmail.com>
2021-11-02 08:56:00 -0400

    add logging

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world, from pipeline!",
+            "message": "hello world, from pipeline (master)!",
             # "location": ip.text.replace("\n", "")
         }),
     }
