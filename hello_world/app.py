03a3aee8837cb0bae82cc84e646f4771de4a5711
Nevaan Perera <nevaan9@gmail.com>
2021-12-08 19:08:47 -0500

    take 4

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "SEPARATE ACCOUNTS! Take 3",
+            "message": "SEPARATE ACCOUNTS! Take 4",
             # "location": ip.text.replace("\n", "")
         }),
     }
