ab2f88892658cc051be023246522a3a42bcdd21d
Nevaan Perera <nevaan9@gmail.com>
2022-01-13 16:21:05 -0500

    version test 1

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "SEPARATE ACCOUNTS! Take 4",
+            "message": "VERSION TEST: 1",
             # "location": ip.text.replace("\n", "")
         }),
     }
