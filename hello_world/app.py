03c14836eb8c8c241bf87fba2be83df989b17a9b
Nevaan Perera <nevaan9@gmail.com>
2022-01-14 10:08:33 -0500

    try AutoPublishAlias: live

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "VERSION TEST: 2",
+            "message": "VERSION TEST: 3",
             # "location": ip.text.replace("\n", "")
         }),
     }
