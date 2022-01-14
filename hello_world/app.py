9b52f710ec7186d4a4fa56b9533ad6122dcaaad3
Nevaan Perera <nevaan9@gmail.com>
2022-01-14 10:21:35 -0500

    v4

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "VERSION TEST: 3",
+            "message": "VERSION TEST: 4",
             # "location": ip.text.replace("\n", "")
         }),
     }
