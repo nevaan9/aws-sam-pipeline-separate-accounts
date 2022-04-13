d4c003c36c276ba7ac52181ba5d64804a40e791c
Nevaan Perera <nevaan9@gmail.com>
2022-04-13 10:03:32 -0400

    change response msg

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "VERSION TEST: 4",
+            "message": "OKAY I CHANGED THIS!",
             # "location": ip.text.replace("\n", "")
         }),
     }
